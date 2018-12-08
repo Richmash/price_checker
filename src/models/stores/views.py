4edd0b7fa6710d4cad2c667abe29b72a12927064
Richard <rimash93@gmail.com>
2018-12-08 12:57:59 +1100

    Added blueprints: user, stores, items and alerts

---
+++ src/models/stores/views.py
@@ -0,0 +1,11 @@
+from flask import Blueprint
+
+__author__ = 'richmash'
+
+
+store_blueprint = Blueprint('stores', __name__)
+
+@store_blueprint.route('/store/<string:name>')
+def store_page():
+    pass
+
